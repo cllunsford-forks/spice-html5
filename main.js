0ea495e4c4a74d11fa74acdca65c01269cb1132a
Chris Lunsford <cllunsford@gmail.com>
2013-12-01 13:20:18 -0500

    Create new functions for abstracting header creation and size checking using a use_mini_header flag.

--- main.js
+++ main.js
@@ -83,12 +83,12 @@ SpiceMainConn.prototype.process_channel_message = function(msg)
             (this.main_init.supported_mouse_modes & SPICE_MOUSE_MODE_CLIENT))
         {
             var mode_request = new SpiceMsgcMainMouseModeRequest(SPICE_MOUSE_MODE_CLIENT);
-            var mr = new SpiceDataHeader();
+            var mr = this.new_header();
             mr.build_msg(SPICE_MSGC_MAIN_MOUSE_MODE_REQUEST, mode_request);
             this.send_msg(mr);
         }
 
-        var attach = new SpiceDataHeader();
+        var attach = this.new_header();
         attach.type = SPICE_MSGC_MAIN_ATTACH_CHANNELS;
         attach.size = attach.buffer_size();
         this.send_msg(attach);
